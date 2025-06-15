9ea0cea1a08108b65e482a2fb8e875b5b73062f2
Louis-VR <134929961+louvr197@users.noreply.github.com>
2025-06-15 19:41:21 +0200

    Add reset and body styles for consistent layout and appearance

---
+++ public/index.php
@@ -0,0 +1,39 @@
+
+<?php
+require_once dirname(__DIR__) . DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'constantes.php';
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$methode = $_SERVER['REQUEST_METHOD'];
+
+if ($uri === '/' && $methode === 'GET')
+{
+    require_once CONTROLLERS . 'AccueilController.php';
+    afficherAccueil();
+}
+else if ($uri === '/contact' ){
+    require_once CONTROLLERS.'ContactController.php';
+    afficherContact();
+}
+else echo $uri;
+
+
+
+
+// /public/index.php
+
+// Importer le routeur.
+// require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR . 'Routeur.php';
+
+// // Définir les routes : chaque route est associée à une méthode HTTP, une URL, un contrôleur et une fonction.
+// $routes = [
+//     configurerRoute('GET', '/', 'AccueilController', 'afficherAccueil'),
+//     configurerRoute('GET', '/contact', 'ContactController', 'afficherContact'),
+//     configurerRoute('POST', '/contact', 'ContactController', 'traiterContact'),
+//     configurerRoute('GET', '/connexion', 'utilisateur/ConnexionController', 'afficherConnexion'),
+//     configurerRoute('POST', '/connexion', 'utilisateur/ConnexionController', 'traiterConnexion'),
+//     configurerRoute('GET', '/erreur', 'ErreurController', 'afficherErreur404'),
+// ];
+
+// // Lancer le routeur.
+// demarrerRouteur($routes);
+?>
