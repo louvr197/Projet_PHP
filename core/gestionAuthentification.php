0dbfc29da7c5939a9b756b42f75cd8bdf5cc1e97
Louis-VR <134929961+louvr197@users.noreply.github.com>
2025-06-20 14:01:41 +0200

    session sécurisée

--- core/gestionAuthentification.php
+++ core/gestionAuthentification.php
@@ -1,5 +1,13 @@
 <?php
-
+ini_set('session.use_strict_mode', 1);
+session_set_cookie_params([
+    'lifetime' => 0,
+    'path' => '/',
+    'domain' => '',
+    'secure' => false, // true en production
+    'httponly' => true,
+    'samesite' => 'Lax'
+]);
 session_start();
 
 function connecter_utilisateur($utilisateurId) {
